4fd5c35c4cb970f8f9e4d2c972347f3b69fd9766
Md. Mahmudul Hasan <117601813+mahmudulmm@users.noreply.github.com>
2022-11-07 04:30:38 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+   $('.counter').counterUp({
+        delay:10,
+         time:1000
+
+    });
+    
+
+   $('.carousel').carousel({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 2000,
+
+    });
+    
+    
+
+
+    
+});
